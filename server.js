2f83636d7dcd1dfc86e1fd9bd8c22c63591a5485
Komal Bhale <30746629+renard404@users.noreply.github.com>
2020-09-29 18:56:27 +0530

    added default app.get

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.json({ message: "server working!" })
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
