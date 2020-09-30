4c0f47252294c046b5e3edb2a937441041eaba89
omkarbhale <66325891+omkarbhale@users.noreply.github.com>
2020-09-30 22:50:29 +0530

    added post apis and changed port number

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
+
+const multer = require("multer");
+var upload = multer({ dest: "/uploads/" })
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -17,6 +22,26 @@ app.get('/', (req, res) => {
     res.send({ message: "server working!" })
 });
 
-app.listen(3000, () => {
+app.post('/add-data', (req, res) => {
+    res.json(req.body);
+})
+
+app.post('/file-upload', upload.single("file"), (req, res) => {
+    console.log(req.file);
+    try {
+        fs.readFile(req.file.path, (error, data) => {
+            if (error) {
+                res.status(500).json({ "error": error });
+            } else {
+                res.send(data);
+            }
+        })
+    } catch (e) {
+        console.log("exception", e);
+        res.json({ "exception": e })
+    }
+})
+
+app.listen(port, () => {
     console.log("Server is listening on port 3000");
 });
