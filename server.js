1742645ee0e7f290452303983baa72d998f42799
Komal Bhale <30746629+renard404@users.noreply.github.com>
2020-09-30 11:53:25 +0530

    send another response

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.json({ message: "server working!" })
+    res.send({ message: "server working!" })
 });
 
 app.listen(3000, () => {
