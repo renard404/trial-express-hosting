7c3aa38e33d21e5aae0623ab4cea743911a99593
omkarbhale <66325891+omkarbhale@users.noreply.github.com>
2020-10-01 20:00:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(bodyParser.json())
 const multer = require("multer");
 var upload = multer({ dest: "/uploads/" })
 
+const files = require("./files");
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
@@ -26,21 +28,7 @@ app.post('/add-data', (req, res) => {
     res.json(req.body);
 })
 
-app.post('/file-upload', upload.single("file"), (req, res) => {
-    console.log(req.file);
-    try {
-        fs.readFile(req.file.path, (error, data) => {
-            if (error) {
-                res.status(500).json({ "error": error });
-            } else {
-                res.send(data);
-            }
-        })
-    } catch (e) {
-        console.log("exception", e);
-        res.json({ "exception": e })
-    }
-})
+app.post('/file-upload', upload.single("file"), files.upload)
 
 app.listen(port, () => {
     console.log("Server is listening on port 3000");
